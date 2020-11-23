06df6b85a75685fa86d7ffdadc1a0d8fe8342c31
Saad Shahid <saad.techgeek@gmail.com>
2020-11-23 20:17:02 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BarChart from './BarChart';
+import PieChart from './PieChart';
+function App(){
+
+  return (
+    <div>
+     <h1>Hello Chart.js</h1>
+     <BarChart/>
+     <PieChart/>
+    </div>
+  );
+}
+
+export default App;
